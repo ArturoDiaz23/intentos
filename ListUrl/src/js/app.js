f45f61961bfdbfc90add31d7269d00eea47467ea
ArturoDiaz23 <ivadrz@outlook.com>
2025-06-26 16:33:06 -0600

    agregar funcionalidad de diálogo para ingresar URL y botón flotante

--- ListUrl/src/js/app.js
+++ ListUrl/src/js/app.js
@@ -1,4 +1,16 @@
 /** Llamado de id home */
-const id_Url = document.getElementById("id_Url");
-const btn_guardar = document.getElementById("btn_guardar");
+let id_Url = document.getElementById('id_Url');
+let btn_guardar = document.getElementById('btn_guardar');
+let btn_add = document.getElementById('btn_add');
+let dialog_url = document.getElementById('dialog_url');
+let msn = document.getElementById('msn');
 
+
+btn_add.addEventListener('click', () => {
+    dialog_url.show();
+});
+
+btn_guardar.addEventListener('click', (e) => {
+    e.preventDefault();
+    msn.innerHTML = 'Ya existe en la Lista';
+})
